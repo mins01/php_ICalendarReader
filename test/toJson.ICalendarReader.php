116bd3e896a9a8626c33d84211c330341afd0f29
PC0092 <mins01.lycos.co.kr@gmail.com>
2020-12-12 11:00:43 +0900

    toJson 과 toJsonAll 제거 json_encode 의 버저 이슈가 있어서

--- test/toJson.ICalendarReader.php
+++ test/toJson.ICalendarReader.php
@@ -6,4 +6,16 @@ $icr = new ICalendarReader();
 // $icr->debug = true;
 header('Content-type: application/json');
 $icr->load(file_get_contents('ics/_test.ics'));
-echo $icr->toJson(JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+
+if(defined('JSON_ERROR_NONE')){ // PHP 5.3 이상
+	$jsonOpt=0;
+	if(defined('JSON_UNESCAPED_UNICODE')){
+		$jsonOpt += JSON_UNESCAPED_UNICODE;
+	}
+	if(defined('JSON_PRETTY_PRINT')){
+		$jsonOpt += JSON_PRETTY_PRINT;
+	}
+	echo json_encode($icr->toObject(),$jsonOpt);
+}else{
+	echo json_encode($icr->toObject());
+}
